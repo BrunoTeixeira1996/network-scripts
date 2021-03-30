c4d3267efc3b4b017654739f130b85f9f6214c89
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2021-03-30 23:09:34 +0100

    rearranged the code

--- script.py
+++ script.py
@@ -3,8 +3,9 @@ import ipaddress
 
 
 # get the VLSM network info
-def getVLSMInfo() -> dict:
-    network_layout = {}
+def getVLSMInfo() -> list:
+    network_layout = []
+
     print("Type 'done' in the network_name input to stop the input")
 
     while True:
@@ -12,88 +13,87 @@ def getVLSMInfo() -> dict:
         if network_name == 'done':
             break
         network_host_number = int(input('Number of hosts: '))
-        network_layout[network_name] = network_host_number
+        network_layout.append({
+            'network_name' : network_name,
+            'network_host_number' : network_host_number,
+            'network_bits' : 0,
+            'network_cidr' : 0,
+            'network_subnetmask' : ''
+        })
 
     return network_layout
 
 # get how many bits every subnet needs
-def getBitsSubnet(network: dict) -> dict:
-    network_bits = network
+def getBitsSubnet(network: list) -> list:
 
-    #fill bits for every subnet
     for i in network:
-        if network[i] == 1:
-            network_bits[i] = 0
-        elif network[i] == 2:
-            network_bits[i] = 2
-        elif network[i] >= 3 and network[i] <= 6:
-            network_bits[i] = 3
-        elif network[i] >= 7 and network[i] <= 14:
-            network_bits[i] = 4
-        elif network[i] >= 15 and network[i] <= 30:
-            network_bits[i] = 5
-        elif network[i] >= 31 and network[i] <= 62:
-            network_bits[i] = 6
-        elif network[i] >= 63 and network[i] <= 126:
-            network_bits[i] = 7
-        elif network[i] >= 127 and network[i] <= 254:
-            network_bits[i] = 8
-        elif network[i] >= 255 and network[i] <= 510:
-            network_bits = 9
-        elif network[i] >= 511 and network[i] <= 1022:
-            network_bits = 10
-
-    return network_bits
+        if i['network_host_number'] == 1:
+            i['network_bits'] = 0
+        elif i['network_host_number'] == 2:
+            i['network_bits'] = 2
+        elif i['network_host_number'] >= 3 and i['network_host_number'] <= 6:
+            i['network_bits'] = 3
+        elif i['network_host_number'] >= 7 and i['network_host_number'] <= 14:
+            i['network_bits'] = 4
+        elif i['network_host_number'] >= 15 and i['network_host_number'] <= 30:
+            i['network_bits'] = 5
+        elif i['network_host_number'] >= 31 and i['network_host_number'] <= 62:
+            i['network_bits'] = 6
+        elif i['network_host_number'] >= 63 and i['network_host_number'] <= 126:
+            i['network_bits'] = 7
+        elif i['network_host_number'] >= 127 and i['network_host_number'] <= 254:
+            i['network_bits'] = 8
+        elif i['network_host_number'] >= 255 and i['network_host_number'] <= 510:
+            i['network_bits'] = 9
+        elif i['network_host_number'] >= 511 and i['network_host_number'] <= 1022:
+            i['network_bits'] = 10
+    return network
 
 # get the mask for every subnet
-def getMaskSubnet(network_bits: dict) -> dict:
-    network_mask = network_bits
-    for i in network_bits:
-        network_mask[i] = 32 - network_mask[i]
+def getMaskSubnet(network: list) -> list:
 
-    return network_mask
+    for i in network:
+        i['network_cidr'] = 32 - i['network_bits']
 
-# transform /mask to original mask
-def transformMask(network_mask: dict) -> dict:
-    network_original = network_mask
-    for i in network_mask:
-        if network_mask[i] == 24:
-            network_original[i] = 0
-        elif network_mask[i] == 25:
-            network_original[i] = '255.255.255.128'
-        elif network_mask[i] == 26:
-            network_original[i] = '255.255.255.192'
-        elif network_mask[i] == 27:
-            network_original[i] = '255.255.255.224'
-        elif network_mask[i] == 28:
-            network_original[i] = '255.255.255.240'
-        elif network_mask[i] == 29:
-            network_original[i] = '255.255.255.248'
-        elif network_mask[i] == 30:
-            network_original[i] = '255.255.255.252'
-        elif network_mask[i] == 31:
-            network_original[i] = '255.255.255.254'
-        elif network_mask[i] == 32:
-            network_original[i] = '255.255.255.255'
+    return network
 
-    return network_original
+# transform cidr to original mask
+def transformMask(network: list) -> list:
+
+    for i in network:
+        if i['network_cidr'] == 24:
+            i['network_subnetmask'] = '255.255.255.0'
+        elif i['network_cidr'] == 25:
+            i['network_subnetmask'] = '255.255.255.128'
+        elif i['network_cidr'] == 26:
+            i['network_subnetmask'] = '255.255.255.192'
+        elif i['network_cidr'] == 27:
+            i['network_subnetmask'] = '255.255.255.224'
+        elif i['network_cidr'] == 28:
+            i['network_subnetmask'] = '255.255.255.240'
+        elif i['network_cidr'] == 29:
+            i['network_subnetmask'] = '255.255.255.248'
+        elif i['network_cidr'] == 30:
+            i['network_subnetmask'] = '255.255.255.252'
+        elif i['network_cidr'] == 31:
+            i['network_subnetmask'] = '255.255.255.254'
+        elif i['network_cidr'] == 32:
+            i['network_subnetmask'] = '255.255.255.255'
+    return network
 
 # calculate the VLSM
 def calcVLSM() -> str:
-    network_unsorted = getVLSMInfo()
-
-    # rearrange networks with more hosts
-    network_sorted = dict(sorted(network_unsorted.items(), key = lambda i : i[1], reverse=True))
+    subnets = getVLSMInfo()
 
     # get the number of bits for every subnet
-    network_bits = getBitsSubnet(network_sorted)
+    network_bits = getBitsSubnet(subnets)
 
     # get the mask for every subnet
     network_mask = getMaskSubnet(network_bits)
     network_original_mask = transformMask(network_mask)
 
     # get the range for a specific subnet (256 - network_mask last octet)
-
+    #fazer um dicionario para cada subrede
 
 
     print(network_original_mask)
@@ -104,6 +104,7 @@ def main():
         calcVLSM()
     else:
         print('bad input')
+
 
 if __name__ == '__main__':
     main()
